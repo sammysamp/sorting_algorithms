222cec80857297af3c24de3928c28a19df309a56
sammysamp <sammysamp@gmail.com>
2021-10-26 12:36:25 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,6 +9,8 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t c, d, swap;
 
+	if (sizeof(array) == 0 || size <= 1)
+		return;
 	for (c = 0 ; c < size - 1; c++)
 	{
 		for (d = 0 ; d < size - c - 1; d++)
